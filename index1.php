c288ed19797e3d75b41122221f0010cef7f94bfa
evgeniakuzina <evgenia.kuzina89@gmail.com>
2018-04-26 16:36:40 +0300

    solution

---
+++ index1.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+1. Наследование - это копирование свойств родительского класса дочерними. Как если бы люди одного рода были все одинаковыми: внешне, внутренне и работали бы на одной и той же работе. Полиморфизм - немного похож на наследование, так как представители каждого класса берут параметры, но модифицируют их. Если уж совсем нафантазировать, то наследование - это однояйцевые близнецы, а полиморфизм - разнояйцевые))
+2. Главное отличие интерфейсов от абстрактного класса, как мне кажется, в том что, интерфейсы содержат только методы, а абстрактный класс может содержать и переменные, при этом методы в интерфейса никак не устанавливаются, как могут в абстрактном классе. Также если реализуется интерфейс в каком-то классе, то нужно обязательно использовать методы, которые в нем описываются, если наследуется абстрактный класс, то это необязательно. Думаю, что интерфейс лучше всего использовать там, где нам нужно гарантированно реализовать какие-то методы, а если в класса метод возможно и не понадобится, лучше использовать абстрактный класс.   
+*/
+
+class SuperClass {
+	public $title;
+	public function getTitle () {
+		echo $this->title;
+		echo "<br>";
+	}
+	
+}
+
+interface HowMuch {
+	public function getPrice();
+}
+
+interface Noises {
+	public function makeNoise($noise);
+}
+
+class Car extends SuperClass implements HowMuch {
+	private $price;
+	public $model;
+	public $color;
+	const WHEELS = 4;
+	public static $carCounter = 0;
+	public function __construct($title, $price, $model, $color) {
+		self::$carCounter++;
+		$this->title = $title;
+		$this->price = $price;
+		$this->model = $model;
+		$this->color = $color;
+	}
+	public function getPrice() {
+			echo $this->price;
+			echo "<br>";
+		}
+}
+
+$bmw = new Car('BMW', 5000, 'X5', 'Белый');
+$bmw->getTitle();
+$toyota = new Car('Toyota', 2000, 'Camry', 'Черный');
+$toyota->getPrice();
+
+class TV extends SuperClass implements HowMuch {
+	private $price;
+	public $diagonal;
+	public $remoteControl;
+	public static $tvCounter = 0;
+	public function __construct($title, $price, $diagonal, $remoteControl) {
+		self::$tvCounter++;
+		$this->title = $title;
+		$this->price = $price;
+		$this->diagonal = $diagonal;
+		$this->remoteControl = $remoteControl;
+	}
+	public function getPrice() {
+			echo $this->price;
+			echo "<br>";
+		}
+}
+
+$samsung = new TV('Samsung', 500, 48, 'Да');
+$toshiba = new TV('Toshiba', 100, 17, 'Нет');
+
+class Pen extends SuperClass implements HowMuch {
+	private $price;
+	public $refillColor;
+	public $penLid;
+	public static $penCounter = 0;
+	public function __construct($title, $price, $refillColor, $penLid) {
+		self::$penCounter++;
+		$this->title = $title;
+		$this->price = $price;
+		$this->refillColor = $refillColor;
+		$this->penLid = $penLid;
+	}
+	public function getPrice() {
+			echo $this->price;
+			echo "<br>";
+		}
+}
+
+$firstPen = new Pen('Parker', 2, 'Голубой', 'Прозрачный', 'Да');
+$secondPen = new Pen('Обычная ручка', 1, 'Красный', 'Белый', 'Нет');
+
+class Duck extends SuperClass implements Noises {
+	public $color;
+	public $age;
+	public $placeOfLiving;
+	public static $duckCounter = 0;
+	public function __construct($title, $color, $age, $placeOfLiving) {
+		self::$duckCounter++;
+		$this->title = $title;
+		$this->color = $color;
+		$this->age = $age;
+		$this->placeOfLiving = $placeOfLiving;
+	}
+	public function makeNoise($noise) {
+		echo $noise;
+		echo "<br>";
+	}
+
+}
+
+$firstDuck = new Duck('Роджер', 'Коричневый', 1, 'Парк Горького');
+$firstDuck->makeNoise('Кря');
+$secondDuck = new Duck('Василиса', 'Сизый', 2, 'Венесуэла');
+
+final class Product extends SuperClass implements HowMuch {
+	private $price;
+	public $category;
+	public $weight;
+	public static $productCounter = 0;
+	public function __construct($title, $category, $price, $weight) {
+		self::$productCounter++;
+		$this->title = $title;
+		$this->category = $category;
+		$this->price = $price;
+		$this->weight = $weight;
+	}
+	public function getPrice() {
+			echo $this->price;
+			echo "<br>";
+		}
+}
+
+$firstProduct = new Product('Тетрадь', 'Канцтовары', 20, 100);
+$secondProduct = new Product('Л.Н.Толстой "Война и Мир"', 'Книги', 100, 600);
+
+
+
+?>
+
+
